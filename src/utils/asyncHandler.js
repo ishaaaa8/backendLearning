f6503b2418dace2bdefea78422ebf8e184517fc9
ishaaaa8 <itsisha4884@gmail.com>
2024-12-21 12:23:03 +0530

    added utilities for api response and error , cookie parser, configuration code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+//this is to create a wrapper for functions
+
+const asyncHandler = (requestHandler) => {
+    (req , res , next) => {
+        Promise.resolve(requestHandler(req , res, next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async ( req, res, next) => {
+//     try{
+//         await fn(req,res,next)
+//     }catch(err){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
