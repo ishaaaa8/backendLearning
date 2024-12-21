18390b9927f6233548b3baa8ea47728a2148ef0c
ishaaaa8 <itsisha4884@gmail.com>
2024-12-22 00:07:09 +0530

    all errors resolved, user router and controller tested , mongoDb connected successfully

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,4 +13,11 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+//routes declaration
+app.use("/api/v1/users", userRouter)
+// /users/registers
+
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //this is to create a wrapper for functions
 
 const asyncHandler = (requestHandler) => {
-    (req , res , next) => {
+    return (req , res , next) => {
         Promise.resolve(requestHandler(req , res, next)).
         catch((err) => next(err))
     }
