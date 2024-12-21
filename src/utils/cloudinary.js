b3a00eae68c0e7df25e10054ad1f819535a1ebd9
ishaaaa8 <itsisha4884@gmail.com>
2024-12-21 21:33:32 +0530

    file uploading using multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+//will give the path to local file
+//file will be uploaded from user using multer which will then be temporarily saved on our server than sent to cloudinary to store on cloud
+
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+//fs -> librray that comes inbuilt with nodejs used for file handling to manage file system
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if( !localFilePath ) return null
+        //upload the file on cloudinary
+        const response =await cloudinary.uploader
+        .upload(localFilePath, {
+            resource_type: "auto"
+        })
+    ////file has been uploaded successfully on cloudinary
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload got failed
+        return null;
+    }
+}
+export { uploadOnCloudinary }
