4cc2d147e7d530f151d3c47f2d092d7d389fa806
ishaaaa8 <itsisha4884@gmail.com>
2024-12-24 11:09:41 +0530

    user watch HIstory pipelines and end point

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -396,6 +396,54 @@ const updateAccountDetails = asyncHandler( async (req, res) => {
     return res.status(200).json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"));  // console.log(channel);
  });
 
+
+const getWatchHistory = asyncHandler( async (req, res) => {
+    //req.user?._id -> string so to convert it to object id we use mongoose.Types.ObjectId
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"))
+});
+
+
 export { registerUser, loginUser,logoutUser ,
     refreshAccessToken, changeCurrentPassword,
     getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile
